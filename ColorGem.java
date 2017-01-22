dde404f241b0bab4e4b732ea7c9719b592c0a68b
ArifRoktim <aroktim@stuy.edu>
2017-01-22 17:19:44 -0500

    Fixed bug where ColorGems dont destroy gems on right and bottom side of board

--- ColorGem.java
+++ ColorGem.java
@@ -14,8 +14,8 @@ public class ColorGem extends SuperGem {
     public ArrayList <Integer[]> special (Gem[][] board, int row, int col){
 	ArrayList <Integer[]> arr = new ArrayList <Integer[]> ();
 	//traverses the entire board to find gems of the same color as the special gem 
-	for ( int i = 0; i < board.length -1; i ++ ) {
-	    for (int j = 0; j <board[i].length -1; j ++) {
+	for ( int i = 0; i < board.length; i ++ ) {
+	    for (int j = 0; j < board[i].length; j ++) {
 		if (board[i][j].equals( board [row][col]) ) {
 		    arr.add (new Integer [] {i, j}); 			    
 		}
